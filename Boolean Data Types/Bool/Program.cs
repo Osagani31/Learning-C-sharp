9a4345bec7facc18fb997e7dd6f0e800401781b5
Osagani31 <naonperera@students.nsbm.ac.lk>
2025-09-14 18:51:48 +0530

    Boolean Data Types

---
+++ Boolean Data Types/Bool/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool value = true;
+
+            bool isfemale = true;
+            Console.WriteLine(isfemale);
+
+            isfemale = false;
+            Console.WriteLine(isfemale);
+
+            Console.ReadLine();
+        }
+    }
+}
