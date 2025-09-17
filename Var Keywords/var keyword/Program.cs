b4f270901b774d6f28874be8f47ac81c85d6deb0
Osagani31 <naonperera@students.nsbm.ac.lk>
2025-09-17 09:14:23 +0530

    Add files via upload

---
+++ Var Keywords/var keyword/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace var_keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var age=23;
+            Console.WriteLine(age);
+
+            var bigNumber = 1234567890123456789;
+            Console.WriteLine(bigNumber);
+
+
+            Console.ReadLine(); 
+
+
+
+
+
+
+
+        }
+    }
+}
