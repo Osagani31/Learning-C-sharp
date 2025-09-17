7601778daf3b6df016a1833053e716adee1a4d41
Osagani31 <naonperera@students.nsbm.ac.lk>
2025-09-17 09:13:09 +0530

    Add files via upload

---
+++ Remainder/Modulus/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = 10;
+            int secondNum = 3;
+
+                Console.WriteLine(firstNum/secondNum); 
+
+            Console.ReadLine();
+
+
+            Console.WriteLine(firstNum % secondNum);
+            Console.ReadLine();
+
+            Console.WriteLine(1000%3);
+            Console.ReadLine();
+        }
+    }
+}
